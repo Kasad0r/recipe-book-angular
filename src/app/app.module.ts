8eb2a1c8a0910a164e9255800efcac0808e509cc
= <dimon.pokemon99@gmail.com>
2020-12-17 21:32:07 +0100

    create main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RecipeListComponent} from './recipies/recipe-list/recipe-list.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {HeaderComponent} from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +31,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
